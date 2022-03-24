864ee6c64e8f9313338ac8c2fa25913d7d70f7de
Deltric <d3ltr1c@gmail.com>
2022-03-24 18:37:58 -0500

    Initial project setup

---
+++ src/main/kotlin/dev/deltric/neature/Neature.kt
@@ -0,0 +1,9 @@
+package dev.deltric.neature
+
+import net.fabricmc.api.ModInitializer
+
+object Neature : ModInitializer {
+    override fun onInitialize() {
+
+    }
+}
---
+++ src/main/kotlin/dev/deltric/neature/client/NeatureClient.kt
@@ -0,0 +1,9 @@
+package dev.deltric.neature.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object NeatureClient : ClientModInitializer {
+    override fun onInitializeClient() {
+
+    }
+}
